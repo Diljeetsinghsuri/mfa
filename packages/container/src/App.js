8a27bbbe2e2109db3a1185d325cd1f4e2042c937
Diljeet Singh Suri <diljeet.suri@gmail.com>
2024-01-04 14:35:42 +0530

    trigger job

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there With version!</h1>
+      <h1>Hi there With version invalidation!</h1>
       <hr />
       <MarketingApp />
     </div>
