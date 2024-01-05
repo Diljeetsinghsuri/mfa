80292fe39c058de1afb33f47a407b55bbd91aebc
Diljeet Singh Suri <diljeet.suri@gmail.com>
2024-01-05 18:04:39 +0530

    added header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+import { BrowserRouter } from 'react-router-dom';
 
 export default () => {
   return (
+    <BrowserRouter>
     <div>
-      <h1>Hi there With version invalidation!</h1>
+      <Header />
       <hr />
       <MarketingApp />
     </div>
+    </BrowserRouter>
   );
 };
