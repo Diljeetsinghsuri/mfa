56e872f484f064d6d7767257d9418190ae37bac0
Diljeet Singh Suri <diljeet.suri@gmail.com>
2024-01-09 19:34:50 +0530

    CSS fixes

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import MarketingApp from './components/MarketingApp';
-import Header from './components/Header';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import MarketingApp from "./components/MarketingApp";
+import Header from "./components/Header";
+import { BrowserRouter } from "react-router-dom";
+import { createGenerateClassName, StylesProvider } from "@material-ui/core";
 
 export default () => {
+  const generateClassName = createGenerateClassName({
+    productionPrefix: "co",
+  });
   return (
     <BrowserRouter>
-    <div>
-      <Header />
-      <hr />
-      <MarketingApp />
-    </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <hr />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
